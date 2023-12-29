703a0393e520a8febedddff88065a03d5d34909f
Nikita kadiwike <ninavenikita492@gmail.com>
2023-12-30 00:32:13 +0530

    implement routing and create navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div>
+       <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+        <Link class="nav-link" to="/signup">Signup</Link>
+        <Link class="nav-link" to="/login">Login</Link>
+
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/views/Signup/Signup.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+
+function Signup() {
+  return (
+    <div>
+      <Navbar />
+      Signup
+    </div>
+  );
+}
+
+export default Signup;
